d0d22402387ef7a3901d8e9021b11672cb89c3f2
JonOuellette <jonathan.t.ouellette@gmail.com>
2024-01-31 16:41:06 -0800

    Build: React Classes Jokes assigment.  Converted function based react components to class based.

---
+++ src/Joke.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './Joke.css';
+
+class Joke extends Component {
+    render() {
+        return (
+            <div className="Joke">
+                <div className="Joke-votearea">
+                  <button onClick={() => this.props.vote(this.props.id, 1)}>
+                    <i className="fas fa-thumbs-up" />
+                  </button>
+
+                  <button onClick={() => this.props.vote(this.props.id, -1)}>
+                    <i className="fas fa-thumbs-down" />
+                  </button>
+
+
+                    {this.props.votes}
+                </div>
+
+                <div className="Joke-text">{this.props.text}</div>
+            </div>
+        );
+    }
+}
+
+export default Joke;
